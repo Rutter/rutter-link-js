f4db7d964e7472758a337de49c2cd4ba47277a22
cyrieu <cyrieu@gmail.com>
2020-12-13 18:01:22 -0800

    Add a handler for script loading

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,31 @@
-const callback = () => {
+const exampleCallback = () => {
   console.log("I JUST FINISHED LOADING");
 };
 
-var scriptTag = document.createElement("script");
-scriptTag.async = true;
-scriptTag.type = "text/javascript";
-scriptTag.src = "https://unpkg.com/@rutter/rutter-link@latest";
-scriptTag.onload = callback;
-
-var scripts = document.getElementsByTagName("script");
+const rutterScriptTag = document.createElement("script");
+rutterScriptTag.async = true;
+rutterScriptTag.type = "text/javascript";
+rutterScriptTag.src = "https://unpkg.com/@rutter/rutter-link@latest";
+const scripts = document.getElementsByTagName("script");
 if (scripts.length > 0) {
-  scripts[0].parentNode.insertBefore(scriptTag, scripts[0]);
+  scripts[0].parentNode.insertBefore(rutterScriptTag, scripts[0]);
 } else {
-  document.body.appendChild(scriptTag);
+  document.body.appendChild(rutterScriptTag);
 }
+
+var RutterLoader = {
+  loadScript: function (callback: any = exampleCallback) {
+    var scriptTag = document.createElement("script");
+    scriptTag.async = true;
+    scriptTag.type = "text/javascript";
+    scriptTag.src = "https://unpkg.com/@rutter/rutter-link@latest";
+    scriptTag.onload = callback;
+
+    var scripts = document.getElementsByTagName("script");
+    if (scripts.length > 0) {
+      scripts[0].parentNode.insertBefore(scriptTag, scripts[0]);
+    } else {
+      document.body.appendChild(scriptTag);
+    }
+  },
+};
