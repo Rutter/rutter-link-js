25581a540aa52ada5a44273fa8594eb25c697f3d
cyrieu <cyrieu@gmail.com>
2021-11-11 13:18:33 -0800

    patch: explain command

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ const exampleCallback = () => {
   console.log("FINISHED LOADING");
 };
 
-const rutterScriptTag = document.createElement("script");
+var rutterScriptTag = document.createElement("script");
 rutterScriptTag.async = true;
 rutterScriptTag.type = "text/javascript";
 rutterScriptTag.src = "https://unpkg.com/@rutter/rutter-link@latest";
-const scripts = document.getElementsByTagName("script");
+var scripts = document.getElementsByTagName("script");
 if (scripts.length > 0) {
   scripts[0].parentNode.insertBefore(rutterScriptTag, scripts[0]);
 } else {
@@ -15,15 +15,15 @@ if (scripts.length > 0) {
 
 const RutterLoader = {
   loadScript: function (callback: any = exampleCallback) {
-    const scriptTag = document.createElement("script");
+    var scriptTag = document.createElement("script");
     scriptTag.async = true;
     scriptTag.type = "text/javascript";
     scriptTag.src = "https://unpkg.com/@rutter/rutter-link@latest";
     scriptTag.onload = callback;
 
-    const scripts = document.getElementsByTagName("script");
-    if (scripts.length > 0) {
-      scripts[0].parentNode.insertBefore(scriptTag, scripts[0]);
+    var otherScripts = document.getElementsByTagName("script");
+    if (otherScripts.length > 0) {
+      otherScripts[0].parentNode.insertBefore(scriptTag, otherScripts[0]);
     } else {
       document.body.appendChild(scriptTag);
     }
